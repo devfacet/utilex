6b741eb7c6303cfd967e9909c59b3c87a5d6dc28
cmfatih <cmuratfatih@gmail.com>
2014-04-26 07:10:18 -0400

    Add unit test

--- test/test-all.js
+++ test/test-all.js
@@ -1,84 +1,127 @@
 // Init reqs
 /* jslint node: true */
+/* global describe: false */
+/* global it: false */
 'use strict';
 
-var utilex = require('../'),
-    fs     = require('fs')
-;
+process.argv.push('--arg1', 'arg1Val', '--arg2', '-arg3', 'arg3Val', 'arg4', 'arg5', '-c', 'test/config-test.json');
 
-// Init vars
-var testList = {
-      TIDYCLEAR: false,
-      TIDYTIME: true,
-      TIDYLOG: true,
-      TIDYARGS: true,
-      TIDYCONFIG: true,
-      PATHSEP: true,
-      PATHCUR: true,
-      ENVMODE: true,
-      DIRISWRT: true,
-      HTTPGETFILE: true
-    }
+var utilex = require('../'),
+    fs     = require('fs'),
+    expect = require('chai').expect
 ;
 
 // Tests
-utilex.tidyLog('test-all.js');
 
-// Test for tidyClear
-if(testList.TIDYCLEAR === true) {
-  utilex.tidyLog('tidyClear: This message will not be shown...');
-  utilex.tidyClear();
-  utilex.tidyLog('tidyClear: OK');
-}
+// Test for utilex module
+describe('utilex', function() {
 
-// Test for tidyTime
-if(testList.TIDYTIME === true) {
-  utilex.tidyLog('tidyTime: ' + utilex.tidyTime());
-  utilex.tidyLog('tidyTime: ' + utilex.tidyTime(new Date(1388638740000)));
-}
+  // Test for tidy time
+  describe('tidyTime', function() {
+    var tidyTime1 = utilex.tidyTime(),
+        tidyTime2 = utilex.tidyTime(new Date(1388638740000))
+    ;
 
-// Test for tidyLog
-if(testList.TIDYLOG === true) {
-  utilex.tidyLog('tidyLog: OK');
-}
+    it('tidyTime() should return a time (' + tidyTime1 + ')', function(done) {
+      expect(tidyTime1).to.be.a('string');
+      done();
+    });
 
-// Test for tidyArgs
-if(testList.TIDYARGS === true) {
-  utilex.tidyLog('tidyArgs: ' + JSON.stringify(utilex.tidyArgs()));
-}
-
-// Test for tidyConfig
-if(testList.TIDYCONFIG === true) {
-  utilex.tidyLog('tidyConfig: ' + JSON.stringify(utilex.tidyConfig()));
-}
-
-// Test for pathSep
-if(testList.PATHSEP === true) {
-  utilex.tidyLog('pathSep: ' + utilex.pathSep);
-}
-
-// Test for pathCur
-if(testList.PATHCUR === true) {
-  utilex.tidyLog('pathCur: ' + utilex.pathCur);
-}
-
-// Test for envMode
-if(testList.ENVMODE === true) {
-  utilex.tidyLog('envMode: ' + utilex.envMode);
-}
-
-// Test for dirIsWritable
-if(testList.DIRISWRT === true) {
-  utilex.tidyLog('dirIsWritable: ' + utilex.dirIsWritable(__dirname));
-}
-
-// Test for httpGetFile
-if(testList.HTTPGETFILE === true) {
-  var fp = './nodejs-logo.svg';
-  utilex.httpGetFile('http://nodejs.org/images/logo.svg', fp).then(function() {
-    utilex.tidyLog('httpGetFile: done!');
-    fs.unlinkSync(fp);
-  }, function(err) {
-    utilex.tidyLog('httpGetFile: ' + err);
+    it('tidyTime(new Date(1388638740000)) should return a time (' + tidyTime2 + ')', function(done) {
+      expect(tidyTime2).to.be.a('string');
+      expect(tidyTime2).to.equal('2014-01-01 23:59:00');
+      done();
+    });
   });
-}
+
+  // Test for tidy log
+  describe('tidyLog', function() {
+    var tidyLog = utilex.tidyLog('A log message', false);
+
+    it("tidyLog('A log message') should return a log message (" + JSON.stringify(tidyLog) + ')', function(done) {
+      expect(tidyLog).to.be.a('object');
+      expect(tidyLog).to.have.property('time');
+      expect(tidyLog).to.have.property('message');
+      done(); 
+    });
+  });
+
+  // Test for tidy arguments
+  describe('tidyArgs', function() {
+    var tidyArgs = utilex.tidyArgs();
+
+    it('tidyArgs() should return command line arguments', function(done) {
+      expect(tidyArgs).to.be.a('object');
+      expect(tidyArgs).to.have.property('arg1', 'arg1Val');
+      expect(tidyArgs).to.have.property('arg2', '');
+      expect(tidyArgs).to.have.property('arg3', 'arg3Val');
+      expect(tidyArgs).to.have.property('arg4', '');
+      expect(tidyArgs).to.have.property('arg5', '');
+      expect(tidyArgs).to.have.property('c', 'test/config-test.json');
+      done();
+    });
+  });
+
+  // Test for tidy config
+  describe('tidyConfig', function() {
+    var tidyConfig = utilex.tidyConfig();
+
+    it('tidyConfig() should return config (' + JSON.stringify(tidyConfig) + ')', function(done) {
+      expect(tidyConfig).to.be.a('object');
+      expect(tidyConfig).to.have.property('file', 'test/config-test.json');
+      expect(tidyConfig).to.have.property('config');
+      expect(tidyConfig.config).to.be.a('object');
+      expect(tidyConfig.config).to.have.property('testKey', 'testVal');
+      done();
+    });
+  });
+
+  // Test for dirIsWritable
+  describe('dirIsWritable', function() {
+    var dirIsWritable = utilex.dirIsWritable(__dirname);
+
+    it('dirIsWritable(__dirname) should run without any error (' + dirIsWritable + ')', function(done) {
+      expect(dirIsWritable).to.be.a('boolean');
+      done();
+    });
+  });
+
+  // Test for httpGetFile
+  describe('httpGetFile', function() {
+    var fp = './nodejs-logo.svg';
+
+    it('httpGetFile() should run without any error', function(done) {
+      utilex.httpGetFile('http://nodejs.org/images/logo.svg', fp).then(function(path) {
+        fs.unlink(fp, function(err) {
+          if(!err) {
+            done();
+          } else {
+            done(err);
+          }
+        });
+      }, function(err) {
+        done(err);
+      });
+    });
+  });
+
+  // Test for others
+  describe('others', function() {
+    var tidyConfig = utilex.pathSep;
+
+    it('pathSep should return platform-specific file separator. (' + utilex.pathSep + ')', function(done) {
+      expect(utilex.pathSep).to.be.a('string');
+      done();
+    });
+
+    it('pathCur should return current path. (' + utilex.pathCur + ')', function(done) {
+      expect(utilex.pathCur).to.be.a('string');
+      done();
+    });
+
+    it('envMode should return environment mode. (' + utilex.envMode + ')', function(done) {
+      expect(utilex.envMode).to.be.a('null');
+      done();
+    });
+  });
+});
